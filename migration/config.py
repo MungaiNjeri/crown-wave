7389a0cf3dae99a5dda4aa7f80c31e88267356b2
njeri <njeri255mungai@gmail.com>
2024-11-06 21:58:47 +0300

    change_DB_to_postgres

--- migration/config.py
+++ migration/config.py
@@ -4,33 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_required, current_user
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
+load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["JWT_SECRET_KEY"] = "super-secret"
-app.config["SECRET_KEY"] = (
-    "SECRET_KEY"  # Necessary for session management with Flask-Login
-)
+
+# Configuration using environment variables
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "super-secret")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "super-secret")
 
 # Flask-Mail Configuration
-app.config["MAIL_SERVER"] = (
-    "smtp.gmail.com"  # Replace with your mail server (e.g., Gmail's SMTP server)
-)
-app.config["MAIL_PORT"] = 587  # TLS port (for Gmail)
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
-app.config["MAIL_USERNAME"] = (
-    "maria123mungai@gmail.com"  # Store this in your environment variables
-)
-app.config["MAIL_PASSWORD"] = (
-    "mupd asww alcl nqkg"  # Store this in your environment variables
-)
-app.config["MAIL_DEFAULT_SENDER"] = (
-    "CrownWave",
-    "maria123mungai@gmail.com",
-)  # Optional
-app.config["RESET_TOKEN_EXPIRATION"] = 3600
+app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 587))  # Convert to int
+app.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "True") == "True"
+app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL", "False") == "True"
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER", "CrownWave")
+app.config["RESET_TOKEN_EXPIRATION"] = int(os.getenv("RESET_TOKEN_EXPIRATION", 3600))
+
 # Initialize extensions
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -39,6 +33,4 @@ mail = Mail(app)
 # Initialize Login Manager
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = (
-    "login"  # Redirect to login page if user is not authenticated
-)
+login_manager.login_view = "login"  # Redirect to login page if user is not authenticated
