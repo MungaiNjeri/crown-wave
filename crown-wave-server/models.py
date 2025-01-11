232a22bd784bf33377c9ae5428300bece0559165
nunjayi <epenyash@gmail.com>
2025-01-11 15:14:48 +0300

    created user model

---
+++ crown-wave-server/models.py
@@ -0,0 +1,51 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+import re
+from werkzeug.security import generate_password_hash, check_password_hash
+from decimal import Decimal
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    user_role = db.Column(db.String(32), default="user", nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean, default=True, nullable=False)
+
+
+    @validates('email')
+    def validate_email(self, key, email):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise ValueError("Invalid email address")
+        return email
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if len(username) < 3 or len(username) > 20:
+            raise ValueError("Username must be between 3 and 20 characters")
+        if not re.match(r"^\w+$", username):
+            raise ValueError("Username must contain only letters, numbers, and underscores")
+        return username
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
