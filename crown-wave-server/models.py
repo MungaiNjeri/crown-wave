672eb05e17b71c34a74eca14db297f7fd85759e7
njeri <njeri255mungai@gmail.com>
2025-01-13 11:30:35 +0300

    edited model  file

--- crown-wave-server/models.py
+++ crown-wave-server/models.py
@@ -49,3 +49,14 @@ class User(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f"<User {self.username}>"
+    
+class Account(db.Model):
+    __tablename__ = "account"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False, unique=True)
+    balance = db.Column(db.Float, default=0.0)
+
+    #Relationships
+    user = db.relationship("User", backref="account", uselist=False)
+
