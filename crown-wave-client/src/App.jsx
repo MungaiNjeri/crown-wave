901cef588cc2af50ebecf3b98d1cee2aeadad746
nunjayi <epenyash@gmail.com>
2025-01-22 23:04:44 +0300

    commit App.jsx

--- crown-wave-client/src/App.jsx
+++ crown-wave-client/src/App.jsx
@@ -1,46 +1,37 @@
-import React, { useState } from 'react' 
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Signup from './pages/signup'
-import Login from './pages/login'
-import Dashboard from './pages/dashboard'
-import './App.css'
-import Deposit from './pages/deposit'
-import Withdraw from './pages/withdraw'
-import Transfer from './pages/transfer'
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Deposit from "./pages/deposit";
+import Withdraw from "./pages/withdraw";
+import Transfer from "./pages/transfer";
+import UserProfile from "./pages/userprofile";
+import Product from "./pages/product";
+import ImageUploader from "./pages/upload";
 
-import UserProfile from './pages/userprofile'
-
-import Product from './pages/product'
-import ImageUploader from './pages/upload'
-
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-
-    <>
     <Router>
       <Routes>
-        {/*<Route path="/" element={} />*/}
-        <Route path='/signup' element = {<Signup />}></Route>
-        <Route path='/login' element = {<Login />}></Route>
-        <Route path='/' element = {<Dashboard />}></Route>
-        <Route path='/deposit' element = {<Deposit />}></Route>
-        <Route path='/withdraw' element = {<Withdraw />}></Route>
-        <Route path='/transfer' element = {<Transfer />}></Route>
-
-        <Route path='/userprofile' element = {<UserProfile />}></Route>
-        <Route path ='/product' element={<Product />}></Route>
-        <Route path ='/upload' element={<ImageUploader />}></Route>
-
-
-
+        {/* Define your routes */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/deposit" element={<Deposit />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/upload" element={<ImageUploader />} />
       </Routes>
-    </Router>  
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
